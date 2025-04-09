19ff55520cdffbc3278b9a5a3e15a2c96fa66e84
mm <mohamedmahmoudjunior1@gmail.com>
2025-04-09 23:13:08 +0200

    adding a name variable

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,7 @@
 # starting program in python 
 print("Hello World")
 print("Hello and welcome to the world of python programming")
+
+# creating a varible 
+name: str = "mohamed"
+print(f"Hello {name} and welcome to the world of python programming")
